28d915d7301c72e38cac64babf694389f9473b9a
Faustermannja <faustermannja@gmail.com>
2019-05-07 10:22:01 -0400

    if/else labs

---
+++ iftest2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+ipchk = input('Apply an IP address: ') # this line now prompts the user for input
+
+if ipchk: # if any data is provided, this will test true
+        print('Looks like the IP address was set: ' + ipchk) # indented under if
+
+elif ipchk: # if any data is provided, this will test true
+    print("looks like the IP address was set: " + ipchk) # indented under if
+else: # if data is NOT provided
+    print('You did not provide input.') # indented under else
+
+
